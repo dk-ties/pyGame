47b9da86429538878c23ca051bd8a1cb377e30ad
dk-ties <dk.ties@gmail.com>
2023-08-17 20:58:02 +0200

    Getting a dot to grow, for each 5 gameTick

--- pyPopAim/main.py
+++ pyPopAim/main.py
@@ -9,11 +9,23 @@ HEIGHT = 720
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
 running = True
+gamerounds = 0
+size = 5
+
+
+def randomCords():
+    timesRandom = random.randint(0, 100)
+    x_pos = 250  # random.randint(int(WIDTH / 100)) * timesRandom
+    y_pos = 255  # random.randint(int(HEIGHT / 100)) * timesRandom
 
 
 # Setting up the circle
-def circleCreation(x_pos, y_pos):
-    dot = pygame.draw.circle(screen, (255, 0, 0), (x_pos * 10, y_pos), 5)
+def circleCreation():
+    timesRandom = random.randint(0, 100)
+    x_pos = 250 * timesRandom  # random.randint(int(WIDTH / 100), int(HEIGHT / 100))
+    y_pos = 130 * timesRandom  # random.randint(int(WIDTH / 100), int(HEIGHT / 100))
+
+    dot = (pygame.draw.circle(screen, (255, 0, 0), (x_pos, y_pos), 5),)
     return dot
 
 
@@ -28,15 +40,23 @@ while running:
     screen.fill("purple")
 
     # RENDER YOUR GAME HERE
-    screenDot = []
-    if len(screenDot) < 5:
+    firsdot = (pygame.draw.circle(screen, (255, 0, 0), (250, 175), size),)
+    if gamerounds == 5:
+        size += 3
+    """ screenDot = 0
+    if screenDot < 5:
         x_pos = random.randint(int(WIDTH / 100), 100)
         y_pos = random.randint(int(WIDTH / 100), 100)
-        circleCreation(x_pos, y_pos)
-
+        circleCreation()
+        screenDot += 1
+ """
     # flip() the display to put your work on screen
     pygame.display.flip()
 
-    clock.tick(30)  # limits FPS to 60
+    clock.tick(60)  # limits FPS to 60
+
+    gamerounds += 1
+    if gamerounds > 5:
+        gamerounds = 0
 
 pygame.quit()
